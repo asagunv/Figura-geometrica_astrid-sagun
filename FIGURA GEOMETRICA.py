124515a7d894d5d3a190e1534a0ed7a052b27356
asagunv <161982019+asagunv@users.noreply.github.com>
2024-03-31 18:48:44 -0500

    Add files via upload

---
+++ FIGURA GEOMETRICA.py
@@ -0,0 +1,42 @@
+# DIBUJE LA FIGURA GEOMETRICA DE LA REFERENCIA
+
+from turtle import*
+title("GEOMETRIK_ASTRID")
+bgcolor("black")
+pencolor("white")
+
+for i in range(3):
+    forward(100)
+    right(120)
+pencolor("orange")
+
+for i in range(4):
+    forward(100)
+    right(90)
+pencolor("blue")
+
+for i in range(5):
+    forward(100)
+    left(10)
+    right(82)
+pencolor("yellow")
+
+for i in range(6):
+    forward(100)
+    left(20)
+    right(80)
+pencolor("purple")
+
+for i in range(7):
+    forward(100)
+    left(30)
+    right(82)
+pencolor("green")
+
+for i in range(8):
+    forward(100)
+    left(40)
+    right(85)
+    pencolor("red")
+
+mainloop()
